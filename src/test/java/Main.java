f5f3ddb72901f5fd6af5b9fcdfea806bcfb5d39e
iosi999 <iosi999@rambler.ru>
2019-09-01 16:04:13 +0300

    commit

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,8 +1,9 @@
 public class Main extends Clon {
     public static void main(String[] args) {
-        String m = "ffff";
+String m="маамама";
         System.out.println(m);
-        System.out.println(m);
+        int g=5;
+        int bb=6;
     }
 
 }
