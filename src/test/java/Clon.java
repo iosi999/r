337e31585d844247605a373c049da49885e12841
iosi999 <ana9851717@gmail.com>
2019-09-01 15:03:47 +0300

    Initial commit

---
+++ src/test/java/Clon.java
@@ -0,0 +1,6 @@
+public class Clon {
+    String m="Ma";
+    public static void printco(String me){
+        System.out.println(me);
+    }
+}
---
+++ src/test/java/Main.java
@@ -0,0 +1,8 @@
+public class Main extends Clon {
+    public static void main(String[] args) {
+        String m = "ffff";
+        System.out.println(m);
+        printco(m);
+    }
+
+}
